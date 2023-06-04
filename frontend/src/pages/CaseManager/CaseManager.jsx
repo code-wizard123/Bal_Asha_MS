977411440c98b2ca2e5e8bef5ce23fff63c98ce7
Bhoomika_156 <bhoomikabodddireddy03@gmail.com>
2023-06-04 16:36:09 +0530

    Hamburger Menu and box

---
+++ frontend/src/pages/CaseManager/CaseManager.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './css/Sidebar.css'
+import './css/orphanages.css'
+import orphanage from '../../Images/children-edu.jpg';
+
+
+const CaseManager=()=>{
+    return (
+		<div>
+<nav role="navigation">
+  <div id="menuToggle">
+    <input type="checkbox" />
+
+    <span></span>
+    <span></span>
+    <span></span>
+
+    <ul id="menu">
+      <a href="#"><li>Home</li></a>
+      <a href="#"><li>About</li></a>
+      <a href="#"><li>Info</li></a>
+      <a href="#"><li>Contact</li></a>
+      <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+    </ul>
+  </div>
+</nav><br></br><br></br>
+<section class="shop contain">
+  <h2 class="section-title">Orphanages</h2>
+  <div class="shop-content">
+      <div class="product-box">
+        <img src={orphanage} alt="Orphanage Image" class="product-img"></img>
+        <h2 class="product-title">BalAsha</h2>
+        <span class="price">Not Assigned: 2/3</span><br></br><br></br>
+        <span class="price">Address:  NNP, Goregaon,Mumbai-65</span>
+      </div>
+  </div>
+</section>
+</div>
+	);
+}
+
+// const CaseManager = () => {
+// 	<li className="hambuger-dashboard">
+//         <Sidebar/>
+//   </li>
+// };
+
+
+export default CaseManager;
+
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,3 @@
-export { default as Opening } from './opening/Opening';
-export { default as Landing } from './landing/Landing';
+export { default as Opening } from "./opening/Opening";
+export { default as Landing } from "./landing/Landing";
+export { default as CaseManager } from "./CaseManager/CaseManager";
