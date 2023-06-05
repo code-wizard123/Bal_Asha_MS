c2ef2e156bdcdb1f4cd1b7902a9b03a404807f4c
Raunak Singh Kalsi <98894054+code-wizard123@users.noreply.github.com>
2023-06-05 17:06:59 +0530

    getting employees based on roles

--- backend/routes/childRoute.js
+++ backend/routes/childRoute.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const { getAllChilds,createChild , updateChild, deleteChild,/*, getOneChild, getChildDetails, createChildReview, deleteReview, getChildReviews*/ 
 getChildDetails}=require("../controllers/childController");
-const { isAuthenticatedUser,authorizeRoles } = require('../middleware/auth');
+const { isAuthenticatedEmployee,authorizeRoles } = require('../middleware/auth');
 // const { getAllChilds } = require('../controllers/childController');
 
 const router=express.Router();
@@ -13,9 +13,9 @@ router.route('/admin/child/new').post/*(isAuthenticatedUser,authorizeRoles("admi
 // router.route('/opManager/').(isAuthenticatedUser,authorizeRoles(2), );
 
 // Ground Worker
-router.route('/groundWorker/new').post(isAuthenticatedUser,authorizeRoles(3), createChild);
-router.route('/groundWorker/childs').get(isAuthenticatedUser,authorizeRoles(3), getAllChilds);
-router.route('/groundWorker/getchild/:id').post(isAuthenticatedUser,authorizeRoles(3), getChildDetails);
+router.route('/groundWorker/new').post(isAuthenticatedEmployee,authorizeRoles(3), createChild);
+router.route('/groundWorker/childs').get(isAuthenticatedEmployee,authorizeRoles(3), getAllChilds);
+router.route('/groundWorker/getchild/:id').post(isAuthenticatedEmployee,authorizeRoles(3), getChildDetails);
 
 //Case Manager
 
